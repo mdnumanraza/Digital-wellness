9d61e0b2317ef749cfcd07637a913c3e30c1e5dc
Numan71417 <89739573+Numan71417@users.noreply.github.com>
2023-09-24 07:42:27 +0530

    .

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -13,9 +13,7 @@ const HomePage = () => {
           <div>
             <h2 className="newsletter__title">Consult Psychiatrist</h2>
             <p className="newsletter__description">
-              Don't miss out on your discounts. Subscribe to our email
-              newsletter to get the best offers, discounts, coupons, gifts and
-              much more.
+            It is the journey that takes you to new destinations every day with endless possibilities of life on the back of happiness, energy, and hope. Practo wants to make this journey easy for every Indian and help them live healthier and longer lives.
             </p>
           </div>
 
@@ -32,8 +30,7 @@ const HomePage = () => {
         <div className="newsletter__bg grid">
           <h2 className="newsletter__title">Report CyberBulling</h2>
           <p className="newsletter__description">
-            Don't miss out on your discounts. Subscribe to our email newsletter
-            to get the best offers, discounts, coupons, gifts and much more.
+          All the details you submit will strictly be kept confidential. Once you have submitted your details, our team members will reach out to you on email and guide you with the next steps
           </p>
           <Link to='/report'>
           <button className="button">Report</button>
